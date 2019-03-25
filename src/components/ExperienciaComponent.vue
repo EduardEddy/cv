080074a4604ece1c3b6b1311b5913357f9d484ee
Eduardo Mendoza <eduardo.mendoza@linktic.com>
2019-03-24 23:19:43 -0500

    ajustando experiencia

--- src/components/ExperienciaComponent.vue
+++ src/components/ExperienciaComponent.vue
@@ -4,62 +4,64 @@
             
             <h1 variant="primary" class="titulo">Experiencia</h1>
             <hr>
-            <b-row>
-                <b-col cols="4">
-                    <b-card
-                        title="Card Title"
-                        img-src="https://picsum.photos/600/300/?image=25"
-                        img-alt="Image"
-                        img-top
-                        tag="article"
-                        style="max-width: 20rem;"
-                        class="mb-2"
-                    >
-                        <b-card-text>
-                        Some quick example text to build on the card title and make up the bulk of the card's content.
-                        </b-card-text>
-                        
-                        <b-button v-b-modal.modallg variant="primary">lg modal</b-button>
-                        
-                    </b-card>
-                </b-col>
+            <b-container>
+                <b-row>
+                    <b-col>
+                        <b-card
+                            title="Card Title"
+                            img-src="https://picsum.photos/600/300/?image=25"
+                            img-alt="Image"
+                            img-top
+                            tag="article"
+                            style="max-width: 20rem;"
+                            class="mb-2"
+                        >
+                            <b-card-text>
+                            Some quick example text to build on the card title and make up the bulk of the card's content.
+                            </b-card-text>
+                            
+                            <b-button v-b-modal.modallg variant="primary">lg modal</b-button>
+                            
+                        </b-card>
+                    </b-col>
 
-                <b-col cols="4">
-                    <b-card
-                        title="Card Title"
-                        img-src="https://picsum.photos/600/300/?image=25"
-                        img-alt="Image"
-                        img-top
-                        tag="article"
-                        style="max-width: 20rem;"
-                        class="mb-2"
-                    >
-                        <b-card-text>
-                        Some quick example text to build on the card title and make up the bulk of the card's content.
-                        </b-card-text>
+                    <b-col>
+                        <b-card
+                            title="Card Title"
+                            img-src="https://picsum.photos/600/300/?image=25"
+                            img-alt="Image"
+                            img-top
+                            tag="article"
+                            style="max-width: 20rem;"
+                            class="mb-2"
+                        >
+                            <b-card-text>
+                            Some quick example text to build on the card title and make up the bulk of the card's content.
+                            </b-card-text>
 
-                        <b-button href="#" variant="primary">Go somewhere</b-button>
-                    </b-card>
-                </b-col>
+                            <b-button href="#" variant="primary">Go somewhere</b-button>
+                        </b-card>
+                    </b-col>
 
-                <b-col cols="4">
-                    <b-card
-                        title="Card Title"
-                        img-src="https://picsum.photos/600/300/?image=25"
-                        img-alt="Image"
-                        img-top
-                        tag="article"
-                        style="max-width: 20rem;"
-                        class="mb-2"
-                    >
-                        <b-card-text>
-                        Some quick example text to build on the card title and make up the bulk of the card's content.
-                        </b-card-text>
+                    <b-col>
+                        <b-card
+                            title="Card Title"
+                            img-src="https://picsum.photos/600/300/?image=25"
+                            img-alt="Image"
+                            img-top
+                            tag="article"
+                            style="max-width: 20rem;"
+                            class="mb-2"
+                        >
+                            <b-card-text>
+                            Some quick example text to build on the card title and make up the bulk of the card's content.
+                            </b-card-text>
 
-                        <b-button href="#" variant="primary">Go somewhere</b-button>
-                    </b-card>
-                </b-col>
-            </b-row>
+                            <b-button href="#" variant="primary">Go somewhere</b-button>
+                        </b-card>
+                    </b-col>
+                </b-row>
+            </b-container>
             
         </b-jumbotron>
 
